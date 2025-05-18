4aed97ccb5451ec175185e7c608e2aa9e3d3c30c
Ponte-Matias <matiasponte20@gmail.com>
2025-05-18 15:39:29 -0300

    feat: automatizar descubrir paths y borrar archivos innecesarios

---
+++ descubrir_paths.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo bash scripts/detect_sensors.sh
+
+python scripts/env.detect.py
+
+sudo rm sensors_report.txt report_cpu.txt report_full.txt set_profile.sh config.txt.recommended
+
+sudo rm -rf profiles/ 
