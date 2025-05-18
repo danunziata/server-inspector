f9f98b8b20fc5724b7f34b620434f96348dd99cd
Ponte-Matias <matiasponte20@gmail.com>
2025-05-18 15:48:55 -0300

    fix: python --> python3

--- descubrir_paths.sh
+++ descubrir_paths.sh
@@ -2,7 +2,7 @@
 
 sudo bash scripts/detect_sensors.sh
 
-python scripts/env.detect.py
+python3 scripts/env.detect.py
 
 sudo rm sensors_report.txt report_cpu.txt report_full.txt set_profile.sh config.txt.recommended
 
